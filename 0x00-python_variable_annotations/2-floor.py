461332ce46a81a6d49837499dfb59b5f0d698cb3
James Kariuki <karisjaymo99@gmail.com>
2024-05-02 16:47:52 +0000

    Task 2, Calculate floor of number

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""This module defines floor function"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """Returns the floor of a number"""
+    return (math.floor(n))

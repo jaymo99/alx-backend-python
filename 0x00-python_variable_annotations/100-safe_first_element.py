012f107986d9d868a8566d37f5b42a52abd104af
James Kariuki <karisjaymo99@gmail.com>
2024-05-02 17:38:34 +0000

    Task 10, using typing.Any

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""100-safe_first_element module"""
+
+from typing import Any, Sequence, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """Returns the first element of a list"""
+    if lst:
+        return lst[0]
+    else:
+        return None

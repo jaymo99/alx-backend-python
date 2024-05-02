f16114f2e8aa5da066645d0fd0649a5f455ee721
James Kariuki <karisjaymo99@gmail.com>
2024-05-02 16:44:43 +0000

    Task 1, Concatenate two strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+"""This module defines a string concat function"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenates two strings"""
+    return ("{}{}".format(str1, str2))

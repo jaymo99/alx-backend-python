36c15456d8e2ed4b1e0b0d38c4b9e523e6cc1df7
James Kariuki <karisjaymo99@gmail.com>
2024-05-07 19:15:03 +0000

    Task 1, Async Comprehensions

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+'''
+1-async_comprehension module
+'''
+
+from typing import List
+async_generator = __import__("0-async_generator").async_generator
+
+
+async def async_comprehension() -> List[float]:
+    '''
+    Returns a list of floats
+    yielded by async_generator
+    '''
+    result = [i async for i in async_generator()]
+    return result
